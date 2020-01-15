27c93f154f98dace5039fc41442b61b68b4d5e0e
Adrian Schwizgebel <adrian.schwizgebel@gmail.com>
2020-01-15 17:11:35 +0100

    Added keyboard support and motion readout

--- main/main.ino
+++ main/main.ino
@@ -1,95 +1,89 @@
-// https://www.instructables.com/id/Arduino-LeonardoMicro-as-Game-ControllerJoystick/
-// https://howtomechatronics.com/tutorials/arduino/arduino-and-mpu6050-accelerometer-and-gyroscope-tutorial/
-// https://maker.pro/arduino/tutorial/how-to-interface-arduino-and-the-mpu-6050-sensor
-// https://github.com/MHeironimus/ArduinoJoystickLibrary/tree/version-1.0
-// https://github.com/MHeironimus/ArduinoJoystickLibrary
-
-#include <Joystick.h>
 #include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
+#include <Keyboard.h>
 
-#define SCL 0
-#define SDA 0
-#define LED_STATUS_YELLOW 0
-#define LED_STATUS_GREEN 0
-#define LED_Q 0
-#define LED_E 0
-#define BTN_CAL 0
-#define BTN_EN 0
+#define TRACKER_INTERVAL 10
+#define KEYBOARD_INTERVAL 10
+#define LED_INTERVAL 10
+#define TRACKING_DURATION 500
+#define TRACKING_TICKS (TRACKING_DURATION / TRACKER_INTERVAL)
 
-Joystick_ Joystick;
+unsigned long currentMillis = 0;
 
-const int MPU = 0x68; // MPU6050 I2C address
-float AccX, AccY, AccZ;
-float GyroX, GyroY, GyroZ;
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
-float roll, pitch, yaw;
-float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-float elapsedTime, currentTime, previousTime;
+unsigned long tracker_previousMillis = 0;
+unsigned long keyboard_previousMillis = 0;
+unsigned long led_previousMillis = 0;
 
-// Last state of the button
-int lastButtonState[2] = {0,0}; // q,e
+MPU6050 mpu;
+int16_t ax, ay, az, gx, gy, gz;
+int vx[TRACKING_TICKS] = {0};
+int vy[TRACKING_TICKS] = {0};
+int vz[TRACKING_TICKS] = {0};
+int trackerCounter = 0;
 
 void setup() {
-  Joystick.begin();
-
-  Serial.begin(19200);
-  Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.initialize();
+  if (!mpu.testConnection()) { while (1); }
+  Keyboard.begin();
+  delay(5000);
 }
 
+void tracker(){
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  vx[trackerCounter] =  (gx + 1800) / 150;  // "+300" because the x axis of gyroscope give values about -350 while it's not moving. Change this value if you get something different using the TEST code, chacking if there are values far from zero.
+  vy[trackerCounter] = -(gy -   30) / 150; // same here about "-100"
+  vz[trackerCounter] = -(gz -   30) / 150; // same here about "-100"
+
+  /*Serial.print("gx = ");
+  Serial.print(gx);
+  Serial.print("gy = ");
+  Serial.print(gy);
+  Serial.print(" | gz = ");
+  Serial.print(gz);
+  */
+  Serial.print("        | X = ");
+  Serial.print(vx[trackerCounter]);
+  Serial.print(" | Y = ");
+  Serial.print(vy[trackerCounter]);
+  Serial.print(" | Z = ");
+  Serial.println(vz[trackerCounter]);
+
+  if(trackerCounter >= (TRACKING_TICKS -1)){
+    trackerCounter = 0;
+  }else{
+    trackerCounter++;
+  }
+}
+
+void keyboard(){
+}
+
+void led(){
+}
+
+}
 void loop() {
-  Joystick.setButton(0, 0);
-  delay(2000);
-  Joystick.setButton(0, 1);
-  delay(2000);
-  Joystick.setButton(1, 0);
-  delay(2000);
-  Joystick.setButton(1, 1);
-  delay(2000);
-
-  // === Read acceleromter data === //
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-  // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - 0.58; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
-  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + 1.58; // AccErrorY ~(-1.58)
-  // === Read gyroscope data === //
-  previousTime = currentTime;        // Previous time is stored before the actual time read
-  currentTime = millis();            // Current time actual time read
-  elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
-  Wire.beginTransmission(MPU);
-  Wire.write(0x43); // Gyro data first register address 0x43
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 4 registers total, each axis value is stored in 2 registers
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
-  // Correct the outputs with the calculated error values
-  GyroX = GyroX + 0.56; // GyroErrorX ~(-0.56)
-  GyroY = GyroY - 2; // GyroErrorY ~(2)
-  GyroZ = GyroZ + 0.79; // GyroErrorZ ~ (-0.8)
-  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
-  gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
-  gyroAngleY = gyroAngleY + GyroY * elapsedTime;
-  yaw =  yaw + GyroZ * elapsedTime;
-  // Complementary filter - combine acceleromter and gyro angle values
-  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
+  currentMillis = millis();
+  if (currentMillis - tracker_previousMillis >= TRACKER_INTERVAL) {
+    tracker_previousMillis = currentMillis;
+    tracker();
+  }
+  if (currentMillis - keyboard_previousMillis >= KEYBOARD_INTERVAL) {
+    keyboard_previousMillis = currentMillis;
+    keyboard();
+  }
+  if (currentMillis - led_previousMillis >= LED_INTERVAL) {
+    led_previousMillis = currentMillis;
+    led();
+  }
   
-  // Print the values on the serial monitor
-  Serial.print(roll);
-  Serial.print("/");
-  Serial.print(pitch);
-  Serial.print("/");
-  Serial.println(yaw);
-
+  //Keyboard.print("test");
+  //delay(1000);
+  //Keyboard.press('e');
+  //delay(2000);
+  //Keyboard.releaseAll();
+  //delay(1000000);
 }
